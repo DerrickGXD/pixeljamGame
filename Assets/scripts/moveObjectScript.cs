543704a2f6ce385858611784052771666d20f387
pauldubois98 <pauldubois98@gmail.com>
2017-10-14 14:12:48 +0200

    added weapon (not done)

---
+++ Assets/scripts/moveObjectScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Moves the object in a direction
+/// </summary>
+
+public class moveObjectScript : MonoBehaviour {
+    //speed
+    public Vector2 speed = new Vector2(10, 10);
+
+
+    //direction
+    public Vector2 direction = new Vector2(-1, 0);
+    private Vector2 movement;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //calculation of the movement
+        movement = new Vector2(
+          speed.x * direction.x,
+          speed.y * direction.y);
+    }
+
+    void FixedUpdate() {
+        // Application du mouvement
+        rigidbody2D.velocity = movement;
+    }
+
+}
