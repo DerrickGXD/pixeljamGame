3b16c7608a023532bdf25eebd7b2cf5f85339e41
pauldubois98 <pauldubois98@gmail.com>
2017-10-14 13:41:10 +0200

    added players&moves

--- Assets/scripts/playerMoves.cs
+++ Assets/scripts/playerMoves.cs
@@ -6,21 +6,44 @@ using UnityEngine;
 /// Player Controls
 /// </summary>
 public class playerMoves : MonoBehaviour {
+    //0 - controls
+    public string xAxis = "Horizontal";
+    public string yAxis = "Vertical";
+    
 
     //1 - speed
     public Vector2 speed = new Vector2(50, 50);
 
     //2 - rigid body
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
 
-    //Move
+    //3 - movement
+    private Vector2 movement;
+
     // Use this for initialization
     void Start () {
-		
+        rb = GetComponent<Rigidbody2D>();
 	}
+
+
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        //Get inputs
+        float inputX = Input.GetAxis(xAxis);
+        float inputY = Input.GetAxis(yAxis);
+
+        //calculate the movement
+        movement = new Vector2(
+            speed.x * inputX,
+            speed.y * inputY);
+
+    }
+
+
+    void FixedUpdate()
+    {
+        //Movement
+        rb.velocity = movement;
+    }
 }
