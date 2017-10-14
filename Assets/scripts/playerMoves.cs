3adf4f7b15f2eda3feee7afad8b10d203b2ea71b
pauldubois98 <pauldubois98@gmail.com>
2017-10-14 13:20:28 +0200

    added folders

---
+++ Assets/scripts/playerMoves.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Player Controls
+/// </summary>
+public class playerMoves : MonoBehaviour {
+
+    //1 - speed
+    public Vector2 speed = new Vector2(50, 50);
+
+    //2 - rigid body
+    public Rigidbody2D rb;
+
+    //Move
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
